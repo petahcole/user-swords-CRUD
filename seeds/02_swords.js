fbef159ca7d2935ff804f12b8842db863d2adf0f
cunnellp5 <philip.cunnell@colorado.edu>
2016-12-16 14:53:52 -0700

    seeds yo

--- seeds/02_swords.js
+++ seeds/02_swords.js
@@ -6,24 +6,24 @@ exports.seed = function(knex, Promise) {
         description: "long and pointy, very straight",
         length: 69,
         user_id: 3,
-        img_url:"http://pngimg.com/upload/sword_PNG5499.png"
+        img_url:"https://pngimg.com/upload/sword_PNG5499.png"
       },{
         description: "slightly curved, but very pretty",
         length: 74,
         is_sharp: false,
         user_id: 4,
-        img_url:"http://pngimg.com/upload/sword_PNG5495.png"
+        img_url:"https://pngimg.com/upload/sword_PNG5495.png"
       },{
         description: "short and stubby, easily concealed",
         length: 30,
         user_id: 3,
-        img_url:"http://i561.photobucket.com/albums/ss57/Ashe37/Weapon%20Images/swords.png"
+        img_url:"https://i561.photobucket.com/albums/ss57/Ashe37/Weapon%20Images/swords.png"
       },{
         description: "wooden",
         length: 50,
         is_sharp: false,
         user_id: 4,
-        img_url:"http://www.medievalcollectibles.com/images/Product/large/MC-1609.png"
+        img_url:"https://www.medievalcollectibles.com/images/Product/large/MC-1609.png"
       }]
       return knex("swords").insert(newSwords);
     });
