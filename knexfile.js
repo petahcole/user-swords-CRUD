3c2d0d04532eceef19bd07d3337d640b0e4caf72
petahcole <cbatson89@gmail.com>
2016-12-16 10:53:06 -0700

    :rocket: added remote stuff

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,16 @@
 // Update with your config settings.
 
+require("dotenv").config();
+
 module.exports = {
 
   development: {
     client: 'pg',
     connection: "postgres://localhost/swords-of-the-world"
   },
-
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + "?ssl=true"
+  },
 
 };
